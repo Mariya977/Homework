421e36db3dc6ee2a586bcbd85a5a78f68758aced
Mariya977 <snezok971@yandex.ru>
2022-11-21 19:12:15 +0300

    Task 41 solved

---
+++ Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateArray();
+ShowArray(myArray);
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > 0)
+    count += 1;
+}
+Console.WriteLine($"Кол-во чисел больше 0: {count}");
